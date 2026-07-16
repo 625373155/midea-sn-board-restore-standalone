408dd845e48ffa9d09e27118606734557e8e4204
625373155 <127856886+625373155@users.noreply.github.com>
2026-07-16 15:46:53 +0800

    Add midea_sn_restore/__init__.py

---
+++ midea_sn_restore/__init__.py
@@ -0,0 +1,4 @@
+"""Standalone, offline package generator for owner-authorized Midea AC repairs."""
+
+__version__ = "1.0.0"
+__repository__ = "https://github.com/625373155/midea-sn-board-restore-standalone"
