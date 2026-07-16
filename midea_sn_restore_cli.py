6f83c33458277470af350c4654367ec821ba823b
625373155 <127856886+625373155@users.noreply.github.com>
2026-07-16 15:43:26 +0800

    Add midea_sn_restore_cli.py

---
+++ midea_sn_restore_cli.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Run the standalone Midea AC board-SN restore package tool from source."""
+
+from midea_sn_restore.cli import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+
